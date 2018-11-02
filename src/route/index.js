81d26bf5e62514b9c1b3f700a7a16904e3b6846c
Alexey Reznikov <oligsei.reznikov@gmail.com>
2018-11-02 20:44:07 +0200

    send 204 for favicon

--- src/route/index.js
+++ src/route/index.js
@@ -3,6 +3,9 @@ const Router = require('koa-router')
 const router = new Router()
 
 router.get('/', ctx => ctx.redirect('v1'))
+router.get('/favicon.ico', ctx => {
+  ctx.status = 204
+})
 router.get('/v1', (ctx) => {
   ctx.body = { status: 'success' }
 })
