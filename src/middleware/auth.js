f3752088026a2b017ba88b07c034ec3a2a06a816
dev19881 <47486019+dev19881@users.noreply.github.com>
2019-02-10 15:10:17 +0200

    CORS update

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,8 +1,9 @@
 const { UserModel } = require('../model/user.model')
 
 const headers = {
-  'WWW-Authenticate': 'Basic realm="Restricted Area", charset="UTF-8"',
-  'Access-Control-Allow-Origin': 'http://127.0.0.1:5500'
+  //'WWW-Authenticate': 'Basic realm="Restricted Area", charset="UTF-8"',
+  'Access-Control-Allow-Origin': 'http://127.0.0.1:5500',
+  'Access-Control-Allow-Headers', 'X-Requested-With'
 }
 
 module.exports.auth = () => async (ctx, next) => {
