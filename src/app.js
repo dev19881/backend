908412996263d133385860cc6e253b7d380614ac
dev19881 <47486019+dev19881@users.noreply.github.com>
2019-02-10 22:04:19 +0200

    CORS middleware

--- src/app.js
+++ src/app.js
@@ -18,6 +18,23 @@ const app = new Koa()
 
 //app.use(cors());
 
+// cors middleware
+app.use(async (ctx, next) => {
+    const origin = ctx.get('Origin');
+    if (ctx.method !== 'OPTIONS') {
+      ctx.set('Access-Control-Allow-Origin', origin);
+      ctx.set('Access-Control-Allow-Credentials', 'true');
+      ctx.set('Access-Control-Allow-Headers', 'Content-Type');
+    } else if (ctx.get('Access-Control-Request-Method')) {
+      ctx.set('Access-Control-Allow-Origin', origin);
+      ctx.set('Access-Control-Allow-Methods', 'GET');
+      ctx.set('Access-Control-Allow-Headers', 'Content-Type');
+      ctx.set('Access-Control-Max-Age', '42');
+      ctx.set('Access-Control-Allow-Credentials', 'true');
+    }
+    await next();
+  });
+
 Model.knex(knex)
 
 app.use(ssl)
